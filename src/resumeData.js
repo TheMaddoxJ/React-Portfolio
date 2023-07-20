684e7a418082f13d9b4dec0459545de7b7d3eb0c
TheMaddoxJ <maddoxjohnson7@gmail.com>
2023-07-20 12:50:46 -0400

    final changes to react portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,67 @@
+let resumeData = {
+  name: "Maddox Johnson",
+  role: "Frontend Developer",
+  emailId: "MaddoxJohnson7@gmail.com",
+  socialLinks: [
+    {
+      name: "github",
+      url: "http://github.com/themaddoxj",
+      className: "fa fa-github",
+    },
+  ],
+  aboutme:
+    "Hey there! I'm Maddox Johnson, a passionate front-end web developer based in beautiful Central Florida. I've always been captivated by the intersection of technology and creativity, and my journey as a developer allows me to bring those elements together. With a keen eye for design and a knack for crafting seamless user experiences, I thrive on turning complex concepts into visually stunning and user-friendly websites. Being married and calling Central Florida home adds an extra layer of inspiration to my work, as I draw inspiration from the vibrant culture and natural wonders of the region. I'm proud of the exceptional results I've achieved so far, and I'm constantly driven to further refine my skills and stay ahead of industry trends. With a passion for front-end development and a commitment to continuous growth, I'm excited to make my mark in the ever-evolving world of web development.",
+  address: "Location: Central Florida",
+  email: "MaddoxJohnson7@gmail.com",
+  work: [
+    {
+      CompanyName: "AMR",
+      specialization: "Emergency Medical Technician",
+      YearOfLeaving: "2023",
+      Achievements:
+        "Performing life saving interventions in high stress environments.",
+    },
+    {
+      CompanyName: "Lake Hills Church - Intern",
+      specialization: "Media Production Manager",
+      MonthOfLeaving: "Present",
+      Achievements:
+        "Production of live stream every Sunday, in charge of all video production and editing, and assist in creating graphics for social media.",
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "JavaScript",
+    },
+    {
+      skillname: "Reactjs",
+    },
+  ],
+  portfolio: [
+    {
+      name: "Cats VS Dogs",
+      imgurl: "/images/portfolio/Cats vs Dogs ss.png",
+    },
+    {
+      name: "Password Generator",
+      imgurl: "images/portfolio/Password Generator Screenshot.png",
+    },
+    {
+      name: "Work Day Scheduler",
+      imgurl: "images/portfolio/Work Day Scheduler Screenshot.png",
+    },
+    {
+      name: "Prework Study Guide",
+      imgurl: "images/portfolio/Prework Study Guide screenshot.png",
+    },
+  ],
+};
+
+export default resumeData;
