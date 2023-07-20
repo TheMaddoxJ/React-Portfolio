61b04eb92a2edb7eb5e0004d84d09e75c0e1906f
TheMaddoxJ <maddoxjohnson7@gmail.com>
2023-07-20 14:30:11 -0400

    Updates

---
+++ precache-manifest.017e332ce1e95960b74e23cb9374912a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "22fb41c1db7a4bec5ed1",
+    "url": "/react-portfolio/static/css/main.52ae65ee.chunk.css"
+  },
+  {
+    "revision": "22fb41c1db7a4bec5ed1",
+    "url": "/react-portfolio/static/js/main.22fb41c1.chunk.js"
+  },
+  {
+    "revision": "02feadb2adb76b10ee9b",
+    "url": "/react-portfolio/static/js/1.02feadb2.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "2722b94e93710b26f34f53fa9f707273",
+    "url": "/react-portfolio/index.html"
+  }
+];
